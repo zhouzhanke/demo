e36703d941f51bb186af0f8a763397d86393f3fe
allen zhouzhanke <allen_zhouzhanke@outlook.com>
2022-04-19 21:56:11 +0800

    + mysql for nacos + nacos config demo = scriptes fix

--- demoprovider/src/main/java/demoprovider/demoprovider/DemoproviderApplication.java
+++ demoprovider/src/main/java/demoprovider/demoprovider/DemoproviderApplication.java
@@ -2,19 +2,29 @@ package demoprovider.demoprovider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
+
 @SpringBootApplication
-// @EnableDiscoveryClient
+@EnableDiscoveryClient
 public class DemoproviderApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DemoproviderApplication.class, args);
+    public static void main(String[] args) throws InterruptedException {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(DemoproviderApplication.class, args);
+        while (true) {
+            //当动态配置刷新时，会更新到 Enviroment中，因此这里每隔一秒中从Enviroment中获取配置
+            String userName = applicationContext.getEnvironment().getProperty("user.name");
+            String userAge = applicationContext.getEnvironment().getProperty("user.age");
+            System.err.println("user name :" + userName + "; age: " + userAge);
+            TimeUnit.SECONDS.sleep(1);
+        }
     }
 
     @RestController
@@ -24,5 +34,4 @@ public class DemoproviderApplication {
             return "Hello Nacos Discovery " + string;
         }
     }
-
 }
--- k8s/stop.sh
+++ k8s/stop.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 echo "mysql mysql stop"
-microk8s kubectl delete -f ./deploy/mysql/mysql-local.yaml
+microk8s kubectl delete -f ./k8s/mysql-local.yaml
 
 
 echo "nacos stop"
-microk8s kubectl delete -f ./deploy/nacos/nacos-quick-start.yaml
+microk8s kubectl delete -f ./k8s/nacos-quick-start.yaml
